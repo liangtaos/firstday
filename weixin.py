ed305df71eb4ea7c396942233adece4dbe6f3da1
Liangtao <lt541977844@163.com>
2016-12-07 16:39:06 +0800

    add weixin.py

---
+++ weixin.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#coding:utf8
+import random
+import sys
+import readline
+def calRandomValue(total, num):
+    total = float(total)
+    total = float('%.2f'%total)
+    print total
+    num = int(num)
+    min = 0.01 #基数
+    if(num < 1): 
+        return
+    if num == 1:
+        print "第%d个人拿到红包数为：%.2f" %(num, total)
+        return
+    
+    i = 1
+    while( i < num ):
+        max = total - min*(num- i)
+        k = int((num-i)/2)
+        if num -i <= 2:
+            k = num -i
+        max = max/k
+        monney = random.randint(int(min*100), int(max*100))
+        monney = float(monney)/100
+        total = total - monney
+        print "第%d个人拿到红包数为：%.2f, 余额为: %.2f" %(i, monney, total)
+        i += 1
+    
+    print "第%d个人拿到红包数为：%.2f, 余额为: %.2f" %(i, total, 0.0)
+if __name__ == "__main__":
+    total = raw_input('输入红包总金额:')
+    num = raw_input('输入发红包数量:')
+    calRandomValue(total, num)
